7fa889a4d06d18a417c8e4caa055c5eaaef09668
salmakhatun12345e <ummesalma0056@gmail.com>
2022-05-26 15:34:36 +0600

    order successfully

--- src/pages/Purchase.js
+++ src/pages/Purchase.js
@@ -1,10 +1,14 @@
 // import React, { useEffect, useState } from 'react';
+import { async } from '@firebase/util';
+import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Loading from '../components/Loading';
 
 const Purchase = ({ user }) => {
     const { id } = useParams()
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
     const tool = useQuery('tool', () => fetch(`http://localhost:5000/get-tool/${id}`)
         .then(res => res.json()))
@@ -14,6 +18,33 @@ const Purchase = ({ user }) => {
     }
     const { name, img, price, description, minimum, available } = tool.data
 
+    const onSubmit = async data => {
+        const order = {
+            toolImage: img,
+            orderQty: data.orderQty,
+            price: price,
+            toolName: name,
+            name: user.displayName,
+            email: user.email,
+            address: data.address,
+            phoneNumber: data.phone
+        }
+        console.log(order)
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                "content-type": 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                toast('Order Success')
+            })
+    };
+
     return (
 
         <div class="hero min-h-screen bg-base-100">
@@ -22,7 +53,7 @@ const Purchase = ({ user }) => {
                     <h1 class="text-3xl text-primary font-bold">Purchase the product</h1>
                 </div>
                 <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div class="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} class="card-body">
                         <div className="from-control">
                             <img src={img} alt="" />
                         </div>
@@ -34,37 +65,45 @@ const Purchase = ({ user }) => {
                             <h2>Minimum Order:  {minimum}pcs</h2>
                             <h2>Available Qty:  {available}pcs</h2>
                         </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Order Quantity</span>
+                            </label>
+                            <input {...register("orderQty")} type="number" min={minimum} max={available} defaultValue={minimum} class="input input-bordered" />
+                        </div>
+                        <div className=" text-center mt-2 text-xl">Your Information</div>
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Your Name</span>
                             </label>
-                            <input type="text" value={user.displayName} disabled class="input input-bordered" />
+                            <input {...register("name")} type="text" value={user.displayName} class="input input-bordered" />
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Your Email</span>
                             </label>
-                            <input type="text" value={user.email} disabled class="input input-bordered" />
-                        </div>
-
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Order Quantity</span>
-                            </label>
-                            <input type="number" min={minimum} max={available} placeholder={minimum} class="input input-bordered" />
+                            <input {...register("email")} type="text" value={user.email} class="input input-bordered" />
                         </div>
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Your Address</span>
                             </label>
-                            <input type="text" class="input input-bordered" />
+                            <input {...register("address")} type="text" class="input input-bordered" />
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Phone Number</span>
+                            </label>
+                            <input {...register("number")} type="text" class="input input-bordered" />
                         </div>
 
                         <div class="form-control mt-6">
                             <button class="btn btn-primary">Please Order Now</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
